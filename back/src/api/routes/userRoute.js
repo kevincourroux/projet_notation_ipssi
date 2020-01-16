ba03a76b6aa2435f381c269678ed8390c2aef7c4
COURROUX <k.courroux@ecole-ipssi.net>
2020-01-16 15:12:58 +0100

    Add userroute

--- back/src/api/routes/userRoute.js
+++ back/src/api/routes/userRoute.js
@@ -1,3 +1,10 @@
 module.exports = (app) => {
-
-}
+    const userController = require('../controllers/userController');
+  
+    app.route('/sessions/')
+    .get(userController.get_all_user)
+    .post(userController.create_a_user)
+    .put(userController.update_a_user)
+    .delete(userController.delete_a_user);
+  }
+  
